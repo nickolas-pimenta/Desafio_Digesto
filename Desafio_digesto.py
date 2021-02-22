7a7635c6db41d73c15446e51e94e6654b029166d
nickolas-pimenta <79375722+nickolas-pimenta@users.noreply.github.com>
2021-02-22 07:28:22 -0300

    Versão final comentada

--- Desafio_digesto.py
+++ Desafio_digesto.py
@@ -4,20 +4,30 @@ from lxml import html
 import os 
 import sys
 import json
+# Este comando identifica o repositório em que o arquivo do programa esta guardado e usa o mesmo endereco para salvar os arquivos CSV e JSON
 script_dir = os.path.abspath(os.path.dirname(sys.argv[0]) or '.')
 
-class Crawler_Tabela():  
+
+# Classe do Crawler que contém todos os metodos utilizados
+class Crawler_Tabela(): 
+
+
+# Este metodo extrai as informacoes da pagina alvo 1 e retorna elas no formato de data frame      
     def Extracao_Pagina_Alvo_1(self):
+
         df = pd.DataFrame(columns=['Storage','CPU', 'Memory','Bandwidth', 'Price'])
         resposta = requests.get('https://www.vultr.com/products/cloud-compute/#pricing')
         conteudo_site = html.fromstring(resposta.content)
         
         if resposta.status_code == 200:
+# Aqui o comando seleciona a tabela desejada via xpath
             div_tabela = '//div[@class = "pt__body js-body"]'
             tabela = conteudo_site.xpath(div_tabela)[0]
+# Aqui o comando seleciona as linhas da tabela via xpath
             div_linhas = tabela.xpath('./div')
                 
             for interacao in range(0,len(div_linhas)):
+# A partir daqui os textos da tabela estao sendo selecionados e tratados para serem convertidos em data frame
                 informacoes = []
                 div_content = div_linhas[interacao].xpath('./div[@class = "pt__row-content"]')[0]
 
@@ -34,6 +44,7 @@ class Crawler_Tabela():
             print("não foi possível extrair as informações")
         return df    
     
+# Este metodo extrai as informacoes da pagina alvo 2 e retorna elas no formato de data frame     
     def Extracao_Pagina_Alvo_2(self):
 
         df = pd.DataFrame(columns=['Memory','vCPUs', 'Transfer','SSD Disk', '$/MO'])
@@ -41,10 +52,14 @@ class Crawler_Tabela():
         conteudo_site = html.fromstring(resposta.content)
         
         if resposta.status_code == 200:
+# Aqui o comando seleciona a tabela desejada via xpath
             selecionar_tabela = '//table[@class = "table is-scrollable css-1map1ow is-fullwidth is-striped"]'
             tabela = conteudo_site.xpath(selecionar_tabela)[0]
+# Aqui o comando seleciona as linhas da tabela via xpath
             linhas_tabela = tabela.xpath('.//tr')
+
             for interacao in range(1,len(linhas_tabela)):
+# A partir daqui os textos da tabela estao sendo selecionados e tratados para serem convertidos em data frame             
                 conteudo_tabela = linhas_tabela[interacao].xpath('.//td')
                 informacoes = []
                 informacoes.append(conteudo_tabela[0].text_content())
@@ -54,8 +69,12 @@ class Crawler_Tabela():
                 informacoes.append(conteudo_tabela[5].text_content())
                 
                 df.loc[interacao-1] = informacoes
+        else:
+            print("não foi possível extrair as informações")
         return df
 
+
+# Este metodo salva as informacoes no formato JSON. Ele recebe um data frame, pede para o usuario inserir o nome que dejesa para o arquivo e o salva no mesmo diretorio que o programa esta salvo
     def Salva_Json(self,df):
         print("Insera o nome do arquivo, mas não coloque .json")
         nome = input()
@@ -64,18 +83,22 @@ class Crawler_Tabela():
             json.dump(dic, data,indent=4)
         print("arquivo salvo com sucesso\n")
 
+# Este metodo salva as informacoes no formato CSV. Ele recebe um data frame, pede para o usuario inserir o nome que dejesa para o arquivo e o salva no mesmo diretorio que o programa esta salvo
     def Salva_CSV(self,df):
         print("Insera o nome do arquivo, mas não coloque .csv")
         nome = input()
         df.to_csv(script_dir+'/'+nome+'.csv',index=False)
         print("arquivo salvo com sucesso\n")
 
+
+# Este metodo recebe um data frame e printa suas informaçoes.
     def Print_informacoes(self,df):
         print(df)
         print("")
           
 
-    def Acoes(self):
+# Este metodo executa o menu que o usuario interage e controla quando um determinado metodo deve ser chamado
+    def Menu(self):
         df_pagina1 = self.Extracao_Pagina_Alvo_1()
         df_pagina2 = self.Extracao_Pagina_Alvo_2()
         if not df_pagina1.empty and not df_pagina2.empty:
@@ -107,5 +130,6 @@ class Crawler_Tabela():
         
 
 if __name__ == "__main__":
+# Aqui cria-se uma instancia da classe e executa o metodo Acoes
     Extrator_de_Dados = Crawler_Tabela()
-    Extrator_de_Dados.Acoes()
+    Extrator_de_Dados.Menu()
